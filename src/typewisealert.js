b07d203c3f75a127573e43a30cdcd4e28b6eb04e
njp1cob <p.anjana@in.bosch.com>
2024-08-26 09:35:40 +0530

    Update typewisealert.js

--- src/typewisealert.js
+++ src/typewisealert.js
@@ -1,26 +1,5 @@
+const { inferBreach, classifyTemperatureBreach } = require('./breachClassification');
 
-
-function inferBreach(value, lowerLimit, upperLimit) {
-  if (value < lowerLimit) {
-    return 'TOO_LOW';
-  }
-  if (value > upperLimit) {
-    return 'TOO_HIGH';
-  }
-  return 'NORMAL';
-}
-
-function classifyTemperatureBreach(coolingType, temperatureInC) {
- 
-  
-   const coolingLimits = {
-        'PASSIVE_COOLING': { lowerLimit: 0, upperLimit: 35 },
-        'HI_ACTIVE_COOLING': { lowerLimit: 0, upperLimit: 45 },
-        'MED_ACTIVE_COOLING': { lowerLimit: 0, upperLimit: 40 }
-    };
- const { lowerLimit, upperLimit } = coolingLimits[coolingType] || {};
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
-}
 
 function checkAndAlert(alertTarget, batteryChar, temperatureInC) {
   const breachType = classifyTemperatureBreach(batteryChar['coolingType'], temperatureInC);
