5fcec48331ed018b6a1aeb89dc0a1b96658ab9db
njp1cob <p.anjana@in.bosch.com>
2024-08-26 23:12:21 +0530

    Update typewisealert.test.js

--- test/typewisealert.test.js
+++ test/typewisealert.test.js
@@ -16,3 +16,13 @@ it('infers a higher value in PASSIVE_COOLING as TOO_HIGH', () => {
 it('infers a normal value as NORMAL', () => {
   expect(alerts.classifyTemperatureBreach('MED_ACTIVE_COOLING', 20)).equals('NORMAL');
 });
+
+it('infers a value lower than the minimum as TOO_LOW', () => {
+  expect(alerts.inferBreach(20, 0, 35)).equals('NORMAL');
+});
+it('infers a boundary value with low value', () => {
+  expect(alerts.inferBreach(0, 0, 100)).equals('TOO_LOW');
+});
+it('infers a boundary value with high value', () => {
+  expect(alerts.inferBreach(50, 0, 50)).equals('NORMAL');
+});
