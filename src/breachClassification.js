a9ead352d28efcc4b74b9e535d1cf638d0ee0cb5
njp1cob <p.anjana@in.bosch.com>
2024-08-26 09:28:42 +0530

    Update breachClassification.js

--- src/breachClassification.js
+++ src/breachClassification.js
@@ -1,1 +1,22 @@
 
+function inferBreach(value, lowerLimit, upperLimit) {
+  if (value < lowerLimit) {
+    return 'TOO_LOW';
+  }
+  if (value > upperLimit) {
+    return 'TOO_HIGH';
+  }
+  return 'NORMAL';
+}
+
+function classifyTemperatureBreach(coolingType, temperatureInC) {
+ 
+  
+   const coolingLimits = {
+        'PASSIVE_COOLING': { lowerLimit: 0, upperLimit: 35 },
+        'HI_ACTIVE_COOLING': { lowerLimit: 0, upperLimit: 45 },
+        'MED_ACTIVE_COOLING': { lowerLimit: 0, upperLimit: 40 }
+    };
+ const { lowerLimit, upperLimit } = coolingLimits[coolingType] || {};
+  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+}
