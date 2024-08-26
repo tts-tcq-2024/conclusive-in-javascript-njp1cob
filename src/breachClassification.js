247c3e60cc327e378fde67cd0a2e93f839de6bf6
njp1cob <p.anjana@in.bosch.com>
2024-08-26 09:49:32 +0530

    Update breachClassification.js

--- src/breachClassification.js
+++ src/breachClassification.js
@@ -1,4 +1,4 @@
-
+const { batteryTypeVal, temperaTureInc } = require('./getColorFromPairs');
 function inferBreach(value, lowerLimit, upperLimit) {
   if (value < lowerLimit) {
     return 'TOO_LOW';
@@ -9,7 +9,7 @@ function inferBreach(value, lowerLimit, upperLimit) {
   return 'NORMAL';
 }
 
-function classifyTemperatureBreach(coolingType, temperatureInC) {
+function classifyTemperatureBreach(batteryTypeVal, temperaTureInc) {
  
   
    const coolingLimits = {
@@ -17,7 +17,7 @@ function classifyTemperatureBreach(coolingType, temperatureInC) {
         'HI_ACTIVE_COOLING': { lowerLimit: 0, upperLimit: 45 },
         'MED_ACTIVE_COOLING': { lowerLimit: 0, upperLimit: 40 }
     };
- const { lowerLimit, upperLimit } = coolingLimits[coolingType] || {};
+ const { lowerLimit, upperLimit } = coolingLimits[batteryTypeVal] || {};
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
