8cc873a007c4403ad6375d0e4c97b93a9c5eb2fc
njp1cob <p.anjana@in.bosch.com>
2024-08-26 13:45:07 +0530

    Update typewisealert.test.js

--- test/typewisealert.test.js
+++ test/typewisealert.test.js
@@ -4,3 +4,7 @@ const {expect} = require('chai');
 it('infers a value lower than the minimum as TOO_LOW', () => {
   expect(alerts.inferBreach(20, 50, 100)).equals('TOO_LOW');
 });
+
+it('infers a value lower than the minimum as TOO_LOW', () => {
+  expect(alerts.classifyTemperatureBreach('HI_ACTIVE_COOLING', 50)).equals('TOO_HIGH');
+});
